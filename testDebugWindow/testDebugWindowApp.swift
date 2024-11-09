b2e022766228cceef6fbd574ce587900574a78d2
1ris_W <ediath462@gmail.com>
2024-11-10 05:30:33 +0800

    Initial Commit

---
+++ testDebugWindow/testDebugWindowApp.swift
@@ -0,0 +1,17 @@
+//
+//  testDebugWindowApp.swift
+//  testDebugWindow
+//
+//  Created by Iris on 2024-11-10.
+//
+
+import SwiftUI
+
+@main
+struct testDebugWindowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
